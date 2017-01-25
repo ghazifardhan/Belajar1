c483b3a23d272b7a332a75dc09c03f2fd5fe4482
Ghazi Fadil Ramadhan <ghazi@markibid.com>
2017-01-25 17:48:26 +0700

    change main activity

--- app/src/main/java/com/example/belajar/MainActivity.java
+++ app/src/main/java/com/example/belajar/MainActivity.java
@@ -1,31 +1,44 @@
 package com.example.belajar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.;*
+
 public class MainActivity extends AppCompatActivity {
+
     private Button button;
-    private TextView textView;
-    private String message = "this is string";
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+
         logic();
     }
 
     private void logic() {
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                textView.setText(message);
+            @Override
+            public void onClick(View view) {
+                double height = Double.parseDouble(textView1.getText().toString());
+                double weight = Double.parseDouble(textView2.getText().toString());
+                double area = height*weight;
+                textView3.setText(area+"");
             }
         });
+    }
 
+    private void initUI(){
+        button = (Button) findViewById(R.id.button1);
+        textView1 = (TextView) findViewById(R.id.editText1);
+        textView2 = (TextView) findViewById(R.id.editText2);
+        textView3 = (TextView) findViewById(R.id.editText3);
     }
 
 }
